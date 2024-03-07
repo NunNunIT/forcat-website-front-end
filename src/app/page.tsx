4fedb95975db3c1033e7f714d7377a6a3ce51915
Kharkizi <vietkha1975@gmail.com>
2024-03-07 20:11:14 +0700

    Add Slider and Categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import React from "react"
+import Carousel from 'react-bootstrap/Carousel';
 import { CustomerCarousel } from "@/components";
-
+import {CustomerSlider} from "@/components"
+import {CustomerCategories} from "@/components"
 // use css
 import "./page.css";
 
 export default function Home() {
   return (
     <>
-      <CustomerCarousel></CustomerCarousel>
+      {/* <CustomerSlider></CustomerSlider>
+      <CustomerCategories></CustomerCategories> */}
+      {/* <CustomerCarousel></CustomerCarousel> */}
       <main className="content-container">This is main content</main>
     </>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as CustomerCarousel } from "./customer/carousel";
+export { default as CustomerSlider } from "./customer/slider/CustomerSlider";
+export {default as CustomerCategories} from "./customer/categories/CustomerCategories";
