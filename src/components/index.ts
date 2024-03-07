628daab67850d55b39f5c22bf242081171024bec
Kharkizi <vietkha1975@gmail.com>
2024-03-07 21:35:48 +0700

    Update name categories and slider

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { default as CustomerCarousel } from "./customer/carousel";
-export { default as CustomerSlider } from "./customer/slider/CustomerSlider";
-export {default as CustomerCategories} from "./customer/categories/CustomerCategories";
+export { default as CustomerSlider } from "./customer/slider";
+export {default as CustomerCategories} from "./customer/categories";
