7e82fd972c5eb380ab6a23e71b0eec9b065bb731
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-03-09 04:51:36 +0700

    refactor: export interfaces

---
+++ src/types/index.tsx
@@ -0,0 +1,60 @@
+export interface ProductProps {
+  id: string,
+  name: string,
+  rating: number,
+  price: number,
+}
+
+export interface SubCategoryProps {
+  id: string,
+  name: string,
+  products: ProductProps[],
+}
+
+export interface CategoryProps {
+  id: string,
+  name: string,
+  iconData?: string,
+  url?: string,
+  subCategories?: SubCategoryProps[]
+}
+
+export interface HeaderLinkProps {
+  title: string,
+  url: string,
+  iconData?: string,
+  className?: string,
+  children?: React.ReactNode,
+}
+
+export interface HeaderMenuProps {
+  categories: CategoryProps[],
+  links: HeaderLinkProps[],
+}
+
+export interface HeaderMenuProductItemProps {
+  id: string,
+  name: string,
+  rating: number,
+  price: number,
+  price__discount?: number,
+  category_id: string,
+  subCategory_id: string,
+}
+
+export interface HeaderMenuSubCategoryItemProps {
+  id: string,
+  title: string,
+  url_img: string,
+  length_products: number,
+  products: ProductProps[],
+  children?: React.ReactNode,
+}
+
+export interface HeaderMenuCategoryItemProps {
+  id: string,
+  title: string,
+  url?: string,
+  iconData?: string,
+  children?: React.ReactNode,
+}
