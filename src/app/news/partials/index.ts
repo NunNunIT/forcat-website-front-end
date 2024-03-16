c43c161eedd2d5a5fb097c926c749bb35a73a90c
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-03-16 08:43:07 +0700

    add: add news

---
+++ src/app/news/partials/index.ts
@@ -0,0 +1,1 @@
+export {default as ArticleSummary} from "./article-summary"
--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -82,3 +82,29 @@ interface IHeaderMenuCategoryItemProps {
   iconData?: string;
   children?: React.ReactNode;
 }
+
+interface IArticleDescriptionContentPRops {
+  type: string;
+  content: string;
+}
+
+interface IArticleDescriptionMediaPRops {
+  type: string;
+  url: string;
+  alt: string;
+  caption: string;
+}
+
+interface IArticleProps {
+  article_id: string,
+  article_name: string,
+  article_type: string,
+  article_short_description: string,
+  article_description?: (IArticleDescriptionContentPRops | IArticleDescriptionMediaPRops)[]
+  article_info: {
+    author: string,
+    published_date: string,
+  },
+  article_date: string,
+  article_slug?: string[],
+}
