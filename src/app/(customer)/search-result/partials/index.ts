2b1817e521265017632673df481c1d4b3397fd4a
Phan Nguyen Hai Yen <pnhaiyen520@gmail.com>
2024-03-26 10:44:34 +0700

    add /search-result

---
+++ src/app/(customer)/search-result/partials/index.ts
@@ -0,0 +1,3 @@
+export { default as SearchResultFilter } from "./search-filter";
+export { default as SearchResultSort } from "./search-sort";
+export { default as SearchResultHeadingMobile } from "./search-heading--mobile";
