c3943f131562b714e59bf921b3a8c5dc117f52cf
Hồng Nhung <145759907+NunNunIT@users.noreply.github.com>
2024-03-30 21:46:39 +0700

    Update commonConst.ts

--- src/utils/commonConst.ts
+++ src/utils/commonConst.ts
@@ -1,4 +1,4 @@
 // File: utils/common.ts
 
-export const BACKEND_URL: string = "http://localhost:8080/api";
+export const BACKEND_URL: string = "http://localhost:8080/";
 export const commonVariable2: number = 42;
