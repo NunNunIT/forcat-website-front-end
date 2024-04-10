a12906888352730fef17a37cc0547ce4ffb68fa1
Kharkizi <vietkha1975@gmail.com>
2024-04-10 22:53:55 +0700

    Fetch data for Notifications
    Lỗi khi truyền dữ liệu vào trang page.tsx từ dòng 127 - 149. Khi truyền dữ liệu thì nó bị trắng trang.

--- src/app/(customer)/notifications/page.tsx
+++ src/app/(customer)/notifications/page.tsx
@@ -1,109 +1,119 @@
-"use client"
+"use client";
 
 // import libs
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, useState, Suspense } from "react";
 import NotFound from "@/app/not-found";
 
 // import partials, components
 import { CustomerNotificationItem } from "./partials";
 
+// import utils
+import { BACKEND_URL } from "@/utils/commonConst";
+
 // import css
 import "./page.css";
 
-const notificationTypes = ['', 'orders', 'promotions'];
+const notificationTypes = ["", "orders", "promotions"];
 
-const fetchData: INotificationItemProps[] = [
-  {
-    notification_id: "1",
-    notification_title: "Thông báo 1",
-    notification_short_desc: "Thông báo 1",
-    notification_desc: [
-      { type: "h2", content: "Thông báo 1", },
-      { type: "h3", content: "1. heading 2 của thông báo 1", },
-      { type: "p", content: "Nội dung thông báo heading 2 của thông báo 1", },
-      { type: "h3", content: "2. heading 2 của thông báo 1", },
-      { type: "p", content: "Nội dung thông báo heading 2 của thông báo 1", },
-    ],
-    notification_url_img: "/imgs/test.png",
-    notification_type: "orders",
-    notification_date: new Date("2022-01-01"),
-    is_read: false,
-  },
-  {
-    "notification_id": "2",
-    "notification_title": "Thông báo 2",
-    "notification_short_desc": "Thông báo 2",
-    "notification_desc": [
-      { "type": "h2", "content": "Thông báo 2" },
-      { "type": "h3", "content": "1. heading 2 của thông báo 2" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 2" },
-      { "type": "h3", "content": "2. heading 2 của thông báo 2" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 2" }
-    ],
-    "notification_url_img": "/imgs/test.png",
-    "notification_type": "orders",
-    "notification_date": new Date("2022-01-05T00:00:00.000Z"),
-    "is_read": false
-  },
-  {
-    "notification_id": "3",
-    "notification_title": "Thông báo 3",
-    "notification_short_desc": "Thông báo 3",
-    "notification_desc": [
-      { "type": "h2", "content": "Thông báo 3" },
-      { "type": "h3", "content": "1. heading 2 của thông báo 3" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 3" },
-      { "type": "h3", "content": "2. heading 2 của thông báo 3" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 3" }
-    ],
-    "notification_url_img": "/imgs/test.png",
-    "notification_type": "promotions",
-    "notification_date": new Date("2022-01-10T00:00:00.000Z"),
-    "is_read": true
-  },
-  {
-    "notification_id": "4",
-    "notification_title": "Thông báo 4",
-    "notification_short_desc": `Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4.
-      Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báoDòngmôtả ngắn về thông báo 4.
-      Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4. Dòng mô tả ngắn về thông báo 4.`,
-    "notification_desc": [
-      { "type": "h2", "content": "Thông báo 4" },
-      { "type": "h3", "content": "1. heading 2 của thông báo 4" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 4" },
-      { "type": "h3", "content": "2. heading 2 của thông báo 4" },
-      { "type": "p", "content": "Nội dung thông báo heading 2 của thông báo 4" }
-    ],
-    "notification_url_img": "/imgs/test.png",
-    "notification_type": "promotions",
-    "notification_date": new Date("2022-01-15T00:00:00.000Z"),
-    "is_read": false
-  },
-]
+// const fetchData = [];
+function fetchData() {
+  return fetch(`${BACKEND_URL}/noti/getNoti/661389d3b2fa25087f9d5455`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch notifications");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("Error fetching notifications:", error);
+      throw error;
+    });
+}
+
+// export default function NotificationPage() {
+//   // const resData = fetchData();
+//   const resData =  fetch(
+//     `${BACKEND_URL}/noti/getNoti/661389d3b2fa25087f9d5455`
+//   ); // Thay thế URL bằng URL thực tế của bạn
+//   console.log("data: ", resData);
+//   const searchParams = useSearchParams();
+//   const [notifications, setNotifications] = useState(fetchData);
+//   const type = searchParams.get("type") ?? "";
+
+//   const handleOnClickReadAll = () => {
+//     const updateNotifications = resData.map((notification) => ({
+//       ...notification,
+//       is_read: true,
+//     }));
+
+//     setNotifications(updateNotifications);
+//   };
+
+//   useEffect(() => {}, [resData]);
+
+//   // if (!notificationTypes.includes(type)) {
+//   //   return NotFound();
+//   // }
+
+//   return (
+//     <>
+//       <section className="notification__content">
+//         <div className="notification__content--top">
+//           <h2 className="notification__title">Thông báo</h2>
+//           <button className="btn_ pri_" onClick={handleOnClickReadAll}>
+//             <span>Đánh dấu tất cả đã đọc</span>
+//           </button>
+//         </div>
+//         <Suspense fallback={"Đang tải dữ liệu..."}>
+//           {resData
+//             .filter(
+//               (notification) =>
+//                 type === "" || notification.notification_type === type
+//             )
+//             .map((notification) => (
+//               <CustomerNotificationItem
+//                 key={notification.notification_id}
+//                 {...notification}
+//               />
+//             ))}
+//         </Suspense>
+//       </section>
+//     </>
+//   );
+// }
 
 export default function NotificationPage() {
+  const [notifications, setNotifications] = useState([]);
+  const resData = fetchData();
+  console.log("data: ", resData);
   const searchParams = useSearchParams();
-  const [notifications, setNotifications] = useState(fetchData);
-  const type = searchParams.get('type') ?? '';
+  const type = searchParams.get("type") ?? "";
+  console.log("type: ", type);
 
   const handleOnClickReadAll = () => {
-    const updateNotifications = notifications.map(notification => ({
-      ...notification,
-      is_read: true,
-    }));
-
-    setNotifications(updateNotifications);
-  }
+    fetchData()
+      .then((data) => {
+        const updateNotifications = data.map((notification) => ({
+          ...notification,
+          is_read: true,
+        }));
+        setNotifications(updateNotifications);
+      })
+      .catch((error) => {
+        console.error("Error updating notifications:", error);
+      });
+  };
 
   useEffect(() => {
-
-  }, [notifications])
+    fetchData().then((data) => {
+      setNotifications(data);
+    });
+  }, []); // Empty dependency array so that this effect runs only once
 
   if (!notificationTypes.includes(type)) {
     return NotFound();
   }
-
 
   return (
     <>
@@ -114,18 +124,33 @@ export default function NotificationPage() {
             <span>Đánh dấu tất cả đã đọc</span>
           </button>
         </div>
-        <Suspense fallback={"Đang tải dữ liệu..."}>
-          {notifications.filter(
-            (notification) => (type === '' || notification.notification_type === type)
-          ).map(
-            (notification) => <CustomerNotificationItem key={notification.notification_id} {...notification} />
-          )}
-        </Suspense>
+        {/* {resData
+          .then((data) => {
+            return data.data.notifications
+              .filter(
+                (notification) =>
+                  type === "" || notification.notification_type === type
+              )
+              .map(
+                (notification) => (
+                  console.log("Data noti:", notification),
+                  (
+                    <CustomerNotificationItem
+                      key={notification._id}
+                      {...notification}
+                    />
+                  )
+                )
+              );
+          })
+          .catch((error) => {
+            console.error("Error filtering notifications:", error);
+            return null; // hoặc xử lý lỗi theo cách thích hợp
+          })} */}
       </section>
     </>
-  )
+  );
 }
-
-async function getNotificationData() {
-  return fetchData;
-}
+// async function getNotificationData() {
+//   return fetchData;
+// }
--- src/app/(customer)/notifications/partials/notification-item/index.tsx
+++ src/app/(customer)/notifications/partials/notification-item/index.tsx
@@ -4,7 +4,10 @@
 import Image from "next/image";
 import classNames from "classnames/bind";
 import { useState } from "react";
-import { convertDateToFormatHHMMDDMMYYYY, isActiveClassWithBool } from "@/utils";
+import {
+  convertDateToFormatHHMMDDMMYYYY,
+  isActiveClassWithBool,
+} from "@/utils";
 
 // import partials, components
 import { CustomerModal } from "..";
@@ -44,43 +47,111 @@ const convertJSONToDOM = (props: {
   );
 };
 
-export default function NotificationItem(props: INotificationItemProps) {
-  const [isRead, setIsRead] = useState<boolean>(props.is_read);
+// export default function NotificationItem() {
+//   // const [isRead, setIsRead] = useState<boolean>(props.is_read);
+//   const [isShowModal, setIsShowModal] = useState<boolean>(false);
+
+//   // const {
+//   //   notification_id, notification_title,
+//   //   notification_short_desc, notification_url_img,
+//   //   notification_desc, notification_date,
+//   // } = props;
+
+//   const handleOnClickRead = () => {
+//     // setIsRead(true);
+//     setIsShowModal(true);
+//   }
+
+//   return (
+//     <>
+//       <div className={cx("notification-item", isActiveClassWithBool(isRead))}>
+//         <div className={cx("notification-item__cover-container")}>
+//           <Image src={notification_url_img}
+//             alt={`Ảnh hiển thị cho thông báo ${notification_title}`} fill
+//           />
+//         </div>
+//         <div className={cx("notification-item__content-wrapper")}>
+//           <h5 className={cx("notification-item__title")}>{notification_title}</h5>
+//           <p className={cx("notification-item__short-description")}>{notification_short_desc}</p>
+//           <div className={cx("notification-item__supported-information")}>
+//             <span className={cx("notification-item__date")}>{convertDateToFormatHHMMDDMMYYYY(notification_date)}</span>
+//             <button onClick={handleOnClickRead} className="btn_ btn--outlined_ pri_">
+//               <span>Xem chi tiết</span>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//       {isShowModal && <CustomerModal onClose={() => setIsShowModal(false)}>
+//         {notification_desc.map((data, index) => convertJSONToDOM(data))}
+//       </CustomerModal>}
+//     </>
+//   )
+// }
+
+export default function NotificationItem(props) {
+  const {
+    _id,
+    notification_name,
+    notification_description,
+    createdAt,
+    is_read, // Thêm is_read vào danh sách các props
+  } = props;
+  console.log("data props:", props);
+
   const [isShowModal, setIsShowModal] = useState<boolean>(false);
 
-  const {
-    notification_id, notification_title,
-    notification_short_desc, notification_url_img,
-    notification_desc, notification_date,
-  } = props;
-
   const handleOnClickRead = () => {
-    setIsRead(true);
     setIsShowModal(true);
-  }
+  };
 
   return (
     <>
-      <div className={cx("notification-item", isActiveClassWithBool(isRead))}>
-        <div className={cx("notification-item__cover-container")}>
-          <Image src={notification_url_img}
-            alt={`Ảnh hiển thị cho thông báo ${notification_title}`} fill
-          />
-        </div>
+      <div className={cx("notification-item")}>
+        <div className={cx("notification-item__cover-container")}></div>
         <div className={cx("notification-item__content-wrapper")}>
-          <h5 className={cx("notification-item__title")}>{notification_title}</h5>
-          <p className={cx("notification-item__short-description")}>{notification_short_desc}</p>
+          <h5 className={cx("notification-item__title")}>
+            {notification_name}
+          </h5>
           <div className={cx("notification-item__supported-information")}>
-            <span className={cx("notification-item__date")}>{convertDateToFormatHHMMDDMMYYYY(notification_date)}</span>
-            <button onClick={handleOnClickRead} className="btn_ btn--outlined_ pri_">
+            <span className={cx("notification-item__date")}>
+              {convertDateToFormatHHMMDDMMYYYY(createdAt)}
+            </span>
+            <button
+              onClick={handleOnClickRead}
+              className="btn_ btn--outlined_ pri_">
               <span>Xem chi tiết</span>
             </button>
           </div>
         </div>
       </div>
-      {isShowModal && <CustomerModal onClose={() => setIsShowModal(false)}>
-        {notification_desc.map((data, index) => convertJSONToDOM(data))}
-      </CustomerModal>}
+      {isShowModal && (
+        <CustomerModal onClose={() => setIsShowModal(false)}>
+          {notification_description}
+        </CustomerModal>
+      )}
     </>
-  )
+  );
+
+  // return (
+  //   <>
+  //     {props.notifications_descriptions}
+  //     "Ten la kha"
+  //     <div className={cx("notification-item__content-wrapper")}>
+  //       {props.notifications_descriptions}
+  //       "Ten la kha"
+  //       <div className={cx("notification-item__supported-information")}>
+  //         <button
+  //           onClick={handleOnClickRead}
+  //           className="btn_ btn--outlined_ pri_">
+  //           <span>{props.notifications_descriptions}</span>
+  //         </button>
+  //       </div>
+  //     </div>
+  //     {isShowModal && (
+  //       <CustomerModal onClose={() => setIsShowModal(false)}>
+  //         {props.notification_description}
+  //       </CustomerModal>
+  //     )}
+  //   </>
+  // );
 }
