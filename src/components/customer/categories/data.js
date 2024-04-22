211cbbeefab41ee407472b4df1023e2a22260dc8
Kharkizi <vietkha1975@gmail.com>
2024-04-22 15:33:48 +0700

    Change image for category card

--- src/components/customer/categories/data.js
+++ src/components/customer/categories/data.js
@@ -2,91 +2,91 @@ export const productData = [
   {
     id: 1,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979753/SEO_Images/Categories/H%E1%BA%A1t-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771928/SEO_Images/Categories/H%E1%BA%A1t-cho-m%C3%A8o.png",
     name: "Hạt",
     description: "Some text about the product..",
   },
   {
     id: 2,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979760/SEO_Images/Categories/Pate-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713772040/SEO_Images/Categories/Pate-cho-m%C3%A8o.png",
     name: "Pate",
     description: "Some text about the product..",
   },
   {
     id: 3,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979747/SEO_Images/Categories/C%E1%BB%8F-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771819/SEO_Images/Categories/C%E1%BB%8F-m%C3%A8o.png",
     name: "Cỏ mèo",
     description: "Some text about the product..",
   },
   {
     id: 4,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979747/SEO_Images/Categories/C%C3%A1t-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771734/SEO_Images/Categories/C%C3%A1t-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
     name: "Cát vệ sinh",
     description: "Some text about the product..",
   },
   {
     id: 5,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979753/SEO_Images/Categories/Nh%C3%A0-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771993/SEO_Images/Categories/Nh%C3%A0-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
     name: "Nhà vệ sinh",
     description: "Some text about the product..",
   },
   {
     id: 6,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979733/SEO_Images/Categories/%E1%BB%94-n%E1%BB%87m-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713772018/SEO_Images/Categories/%E1%BB%94-n%E1%BB%87m-cho-m%C3%A8o.png",
     name: "Nệm, ổ, thảm",
     description: "Some text about the product..",
   },
   {
     id: 7,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979755/SEO_Images/Categories/Nh%C3%A0-c%C3%A2y-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771954/SEO_Images/Categories/Nh%C3%A0-c%C3%A2y-cho-m%C3%A8o.png",
     name: "Nhà cây",
     description: "Some text about the product..",
   },
   {
     id: 8,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979746/SEO_Images/Categories/B%C3%A0n-c%C3%A0o-m%C3%B3ng-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771513/SEO_Images/Categories/B%C3%A0n-c%C3%A0o-m%C3%B3ng-cho-m%C3%A8o.png",
     name: "Bàn cào móng",
     description: "Some text about the product..",
   },
   {
     id: 9,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979732/SEO_Images/Categories/Qu%E1%BA%A7n-%C3%A1o-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713772068/SEO_Images/Categories/Qu%E1%BA%A7n-%C3%A1o-cho-m%C3%A8o.png",
     name: "Quần áo",
     description: "Some text about the product..",
   },
   {
     id: 10,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979734/SEO_Images/Categories/V%C3%B2ng-c%E1%BB%95-d%C3%A2y-d%E1%BA%AFt-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713772095/SEO_Images/Categories/V%C3%B2ng-c%E1%BB%95-d%C3%A2y-d%E1%BA%AFt-m%C3%A8o.png",
     name: "Vòng cổ, dây dắt",
     description: "Some text about the product..",
   },
   {
     id: 11,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979595/SEO_Images/Categories/Balo-%C4%91%E1%BB%B1ng-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771484/SEO_Images/Categories/Balo-%C4%91%E1%BB%B1ng-m%C3%A8o.png",
     name: "Balo, Lồng",
     description: "Some text about the product..",
   },
   {
     id: 12,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979751/SEO_Images/Categories/%C4%90%E1%BB%93-ch%C6%A1i-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771875/SEO_Images/Categories/%C4%90%E1%BB%93-ch%C6%A1i-cho-m%C3%A8o.png",
     name: "Đồ chơi",
     description: "Some text about the product..",
   },
   {
     id: 13,
     imageurl:
-      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1712979758/SEO_Images/Categories/%C4%90%E1%BB%93-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
+      "https://res.cloudinary.com/dmjwq3ebx/image/upload/v1713771899/SEO_Images/Categories/%C4%90%E1%BB%93-v%E1%BB%87-sinh-cho-m%C3%A8o.png",
     name: "Đồ vệ sinh",
     description: "Some text about the product..",
   },
