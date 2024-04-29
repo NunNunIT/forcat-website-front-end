a0db53b7e98d0fe74edecfb7b5d1f73a5d60a398
namtuthien <leducmanh123kt@gmail.com>
2024-04-30 01:07:36 +0700

    add: CategoryPage

---
+++ src/app/(default)/category/components/index.ts
@@ -0,0 +1,1 @@
+export { default as CategoryImage } from "./category-image";
--- src/partials/customer/header/partials/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as CustomerHeaderMain } from "./header-main";
-export { default as CustomerHeaderMenu } from "./header-menu";
-export { default as CustomerHeaderNav } from "./header-nav";
