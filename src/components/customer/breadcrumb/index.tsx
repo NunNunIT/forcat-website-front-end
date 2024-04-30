a85e6bd61ae00c2397911320095cc9ba75418955
Lê Trung Hiếu <54971686+fredle09@users.noreply.github.com>
2024-04-30 18:00:13 +0700

    hotfix(feature/breadcrumb): fix /privacy-policy, /term-of-use

--- src/components/customer/breadcrumb/index.tsx
+++ src/components/customer/breadcrumb/index.tsx
@@ -33,6 +33,8 @@ const decryptNamePage = (namePage: string, prevNamePage: string) => {
     category: { text: "Danh mục sản phẩm", url: "/category" },
     contact: { text: "Liên hệ", url: "/contact" },
     "about-us": { text: "Về chúng tôi", url: "/about-us" },
+    "term-of-use": { text: "Điều khoản chung", url: "term-of-use" },
+    "privacy-policy": { text: "Chính sách bảo mật", url: "privacy-policy" }
   };
 
   if (namePage === "mobile-account") {
