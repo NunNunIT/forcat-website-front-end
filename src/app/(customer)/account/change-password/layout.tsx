ff04d78af3e004a512b8abc9d29be1aab759eb82
Kharkizi <vietkha1975@gmail.com>
2024-04-30 16:34:21 +0700

    Update Metadata

--- src/app/(customer)/account/change-password/layout.tsx
+++ src/app/(customer)/account/change-password/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  title: "Đổi mật khẩu",
   description:
     "Thay đổi mật khẩu của bạn để bảo vệ thông tin cá nhân và tài khoản trên ForCat Shop. Trang này cho phép bạn cập nhật mật khẩu hiện tại và thiết lập mật khẩu mới một cách an toàn",
 };
