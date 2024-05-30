90686f76e2f7d4777aa9ea9a6cfafeb727197888
fredle09 <le197903@gmail.com>
2024-05-30 10:50:01 +0700

    feat(feature/admin/sidebar): update navlinks

--- src/partials/admin/sidebar/index.tsx
+++ src/partials/admin/sidebar/index.tsx
@@ -1,19 +1,40 @@
 // import libs
 import classNames from "classnames/bind";
 
+// import components
+import { buttonVariants } from "@/components/admin/ui/button";
+
 // import styles
 import styles from "./sidebar.module.css";
 import Link from "next/link";
 
 const cx = classNames.bind(styles);
 
+const navLinks = [
+  { href: "/admin/orders", label: "Quản lý đơn hàng" },
+  { href: "/admin/articles", label: "Quản lý bài viết" },
+]
+
 export default function Sidebar() {
-  return <aside className={`${cx("admin-aside")} p-4`}>
+  return <aside className={`${cx("admin-aside")} p-4 flex flex-col gap-2`}>
     <h2>
       Sidebar cho Admin
     </h2>
-    <Link href="/admin/orders">
+    {navLinks.map((item, index) => (
+      <Link
+        key={index}
+        className={`${buttonVariants({ variant: "link", size: "default" })} text-left`}
+        href={item.href}
+      >
+        {item.label}
+      </Link>
+    ))
+    }
+    {/* <Link className={buttonVariants({ variant: "link", size: "md" })} href="/admin/orders">
       Quản lý đơn hàng
     </Link>
+    <Link href="/admin/articles">
+      Quản lý bài viết
+    </Link> */}
   </aside>
 }
